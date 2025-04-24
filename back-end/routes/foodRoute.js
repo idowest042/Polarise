cc81805088479bddbceeeaed7388828c447c3583
Idowu Joseph <idowujo672@gmail.com>
2025-04-24 17:47:06 +0100

    made some changes to the foodroute

--- back-end/routes/foodRoute.js
+++ back-end/routes/foodRoute.js
@@ -1,16 +1,14 @@
 import express from 'express';
-import{addFood, listFood, removeFood} from '../controllers/foodController.js';
+import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
+
 const foodRouter = express.Router();
-const storage = multer.diskStorage({
-    destination: "/uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()} ${file.originalname}`);
-    }
-})
-const upload = multer({ storage: storage });
-foodRouter.post('/addfood',upload.single('image'),addFood)
-foodRouter.get('/list', listFood)
-foodRouter.post('/remove',removeFood)
+
+// Use memory storage instead of disk storage
+const upload = multer({ storage: multer.memoryStorage() });
+
+foodRouter.post('/addfood', upload.single('image'), addFood);
+foodRouter.get('/list', listFood);
+foodRouter.post('/remove', removeFood);
 
 export default foodRouter;
